7a999c853d32cbc766ac875e7918667c15f0a70f
Rohit <rohitfortune@gmail.com>
2026-06-01 22:07:41 +0530

    chore: remove all capacitor related code and dependencies

--- android/app/src/main/java/com/example/onewebapp/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.onewebapp;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/utils/haptics.ts
+++ src/utils/haptics.ts
@@ -1,11 +1,6 @@
-import { Haptics, ImpactStyle } from '@capacitor/haptics';
-import { Capacitor } from '@capacitor/core';
-
 export const triggerHapticLight = async () => {
   try {
-    if (Capacitor.isNativePlatform()) {
-      await Haptics.impact({ style: ImpactStyle.Light });
-    } else if (navigator.vibrate) {
+    if (navigator.vibrate) {
       navigator.vibrate(10);
     }
   } catch (e) {
@@ -16,9 +11,7 @@ export const triggerHapticLight = async () => {
 
 export const triggerHapticMedium = async () => {
   try {
-    if (Capacitor.isNativePlatform()) {
-      await Haptics.impact({ style: ImpactStyle.Medium });
-    } else if (navigator.vibrate) {
+    if (navigator.vibrate) {
       navigator.vibrate(30);
     }
   } catch (e) {
@@ -28,9 +21,7 @@ export const triggerHapticMedium = async () => {
 
 export const triggerHapticError = async () => {
   try {
-    if (Capacitor.isNativePlatform()) {
-      await Haptics.notification({ type: 'ERROR' as any });
-    } else if (navigator.vibrate) {
+    if (navigator.vibrate) {
       navigator.vibrate([30, 50, 30]);
     }
   } catch (e) {
